8618512ad6ee60b129e794fb513f8b3325f877b0
Ilham Arifin <ia3001148@gmail.com>
2023-11-19 22:04:22 +0800

    SAVE1

---
+++ main.js
@@ -0,0 +1,30 @@
+alert(`Hai! Sebelum masuk ke laman WEBPRO, Kita akan bermain "Tebak Angka"`);
+
+let x= parseInt(prompt(`Masukkan angka yang anda inginkan`));
+
+while (!x) {
+    x= parseInt(prompt(`Inputan harus berupa angka!`));
+};
+
+let y=1;
+
+let gues= Math.floor(Math.random()*x+1);
+alert(`Inputan sebelumnya merupakan nilai maksimal interval dari tebakan berikut`)
+let jawaban= parseInt(prompt(`Tebak angka:`));
+while (jawaban !== gues) {
+    y++;
+
+    if (jawaban> gues) {
+        jawaban= parseInt(prompt(`Coba lagi dengan angka yang lebih kecil dari ${jawaban}`));
+    } else {
+        jawaban= parseInt(prompt(`Coba lagi dengan angka yang lebih besar dari ${jawaban}`));
+    }
+};
+
+if (y== 1) {
+    alert(`SELAMAT, Tebakan anda benar dengan hanya satu percobaan. Ampun suhu..!`)
+} else if (y<= 5) {
+    alert(`SELAMAT, Tebakan anda benar setelah ${y} percobaan, yaah lumayanlah..!`)
+} else {
+    alert(`SELAMAT, Tebakan anda benar setelah ${y} percobaan dan itu sangat PAYAH!!`)
+};
